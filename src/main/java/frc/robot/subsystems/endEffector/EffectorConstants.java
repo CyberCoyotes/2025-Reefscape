6a77c5f1243178254e3a130afb656038e44d1483
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-02-23 15:11:29 -0500

    Update INTAKE_ALGAE constant to a fixed value

--- src/main/java/frc/robot/subsystems/endEffector/EffectorConstants.java
+++ src/main/java/frc/robot/subsystems/endEffector/EffectorConstants.java
@@ -11,7 +11,7 @@ public class EffectorConstants {
     public static final double SCORE_SOFT_CORAL = 0.25;
     public static final double SCORE_CORAL_INVERTED = -0.3;
 
-    public static final double INTAKE_ALGAE = INTAKE_CORAL * -1; // Opposite power of INTAKE
+    public static final double INTAKE_ALGAE = -0.40; // Opposite power of INTAKE
     public static final double SCORE_ALGAE = 0.40;
     public static final double HOLD_ALGAE = 0.05; // Low power hold
 
