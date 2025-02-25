beeb9870944fe57c91173ddd8e159e3c81e1072f
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-02-25 10:04:30 -0500

    Refactor RobotContainer for improved readability and organization

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,7 +25,6 @@ import frc.robot.subsystems.TOFSubsystem;
 import frc.robot.subsystems.elevator.ElevatorSubsystem;
 import frc.robot.subsystems.endEffector.EffectorSubsystem;
 import frc.robot.subsystems.wrist.WristSubsystem;
-import frc.robot.subsystems.wrist.WristSubsystem.WristPositions;
 import frc.robot.subsystems.climber.ClimberSubsystem;
 import frc.robot.commands.ClimberCommands;
 import frc.robot.commands.CommandGroups;
@@ -46,11 +45,12 @@ public class RobotContainer {
     private final ClimberCommands climberCommands = new ClimberCommands(climber, wrist);
 
     private final CommandGroups commandGroups = new CommandGroups();
+    
     private final TOFSubsystem m_tof = new TOFSubsystem();
 
-
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second                                                            // max angular velocity
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second //
+                                                                                      // max angular velocity
 
     private final AutoFactory autoFactory;
     private final AutoRoutines autoRoutines;
@@ -83,44 +83,48 @@ public class RobotContainer {
     }
 
     private void configureAutoRoutines() {
-        
-       // autoChooser.addRoutine("Drive Forward", autoRoutines::driveForward);
-       // autoChooser.addRoutine("Center Score", autoRoutines::driveForward);
-       //autoChooser.addRoutine("TwoMeters", autoRoutines::TwoMeters); 
-       autoChooser.addRoutine("ScoreTwoMetersBack", autoRoutines::ScoreTwoMetersBack); 
-       autoChooser.addRoutine("SetupA", autoRoutines::SetupA);
-       autoChooser.addRoutine("BetterSTA", autoRoutines::STA3);
-       autoChooser.addRoutine("STA-L1?", autoRoutines::STAL1);
-       autoChooser.addRoutine("SmashA", autoRoutines::ReefSMASH);
-       autoChooser.addRoutine("SmashB", autoRoutines::ReefSMASH2);
 
-       SmartDashboard.putData("Autonomous", autoChooser);
+        // autoChooser.addRoutine("Drive Forward", autoRoutines::driveForward);
+        // autoChooser.addRoutine("Center Score", autoRoutines::driveForward);
+        // autoChooser.addRoutine("TwoMeters", autoRoutines::TwoMeters);
+        autoChooser.addRoutine("ScoreTwoMetersBack", autoRoutines::ScoreTwoMetersBack);
+        autoChooser.addRoutine("SetupA", autoRoutines::SetupA);
+        autoChooser.addRoutine("BetterSTA", autoRoutines::STA3);
+        autoChooser.addRoutine("STA-L1?", autoRoutines::STAL1);
+        autoChooser.addRoutine("SmashA", autoRoutines::ReefSMASH);
+        autoChooser.addRoutine("SmashB", autoRoutines::ReefSMASH2);
+
+        SmartDashboard.putData("Autonomous", autoChooser);
 
     }
 
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
+
         drivetrain.setDefaultCommand(
-                // Drivetrain will execute this command periodically
-                drivetrain.applyRequest(() -> drive.withVelocityX(-driverController.getLeftY() * MaxSpeed) // Drive
-                                                                                                           // forward
-                                                                                                           // with
-                                                                                                           // negative Y
-                                                                                                           // (forward)
-                        .withVelocityY(-driverController.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                        .withRotationalRate(-driverController.getRightX() * MaxAngularRate) // Drive counterclockwise
-                                                                                            // with negative X (left)
+                // Drivetrain will execute this command periodically // Drive forward with
+                // negative Y (forward)
+                drivetrain.applyRequest(() -> drive.withVelocityX(-driverController.getLeftY() * MaxSpeed)
+                        // Drive left with negative X (left)
+                        .withVelocityY(-driverController.getLeftX() * MaxSpeed)
+                        // Drive counterclockwise with negative X (left)
+                        .withRotationalRate(-driverController.getRightX() * MaxAngularRate)
+
                 ));
 
-        driverController.start().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));        
+        /***********************************************
+         * Driver Controls
+         *
+         ***********************************************/
+        driverController.start().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         /***** Driver Controls *****/
         driverController.start().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         driverController.leftBumper().whileTrue(endEffector.intakeCoral());
         driverController.rightBumper().whileTrue(endEffector.scoreCoral());
-        
+
         driverController.x().onTrue(commandGroups.moveToL2Group(wristCommands, elevatorCommands));
         driverController.y().onTrue(commandGroups.moveToL3Group(wristCommands, elevatorCommands));
 
@@ -132,8 +136,9 @@ public class RobotContainer {
         driverController.povLeft().whileTrue(wristCommands.setStowed());
         driverController.povRight().whileTrue(wristCommands.incrementOut());
 
-        /***** Operator Controls *****/
-
+        /***********************************************
+         * Operator Controls
+         ***********************************************/
         operatorController.leftBumper().whileTrue(endEffector.intakeAlgae());
         operatorController.rightBumper().whileTrue(endEffector.scoreAlgae());
 
