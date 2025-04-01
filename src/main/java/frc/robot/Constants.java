7d781d79a53cec2eafe90610a9d72286b45154bb
Jestin VanScoyoc <31118852+JediScoy@users.noreply.github.com>
2025-04-01 16:56:09 -0400

    Add second effector motor ID to Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,7 +10,8 @@ public class Constants {
     public static final int WRIST_MOTOR_ID = 20;
     public static final int WRIST_ENCODER_ID = 21;
     
-    public static final int EFFECTOR_MOTOR_ID = 22; 
+    public static final int EFFECTOR_MOTOR_ID = 22;
+    public static final int EFFECTOR_MOTOR_TWO_ID = 26; 
 
     public static final int ELEVATOR_FOLLOW_ID = 23;
     public static final int ELEVATOR_LEAD_ID = 24;
